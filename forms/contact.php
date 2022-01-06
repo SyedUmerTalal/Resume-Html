f26b69739e50f2820277a6111996d5f281452879
Syed Umer Talal Bukhari <talal.bukhari@binaryit.co>
2022-01-06 14:39:04 +0500

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,32 @@
+<?php
+  $receiving_email_address = 'umerbukhari555.ub@gmail.com';
+
+  if( file_exists($php_email_form = '/assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library Okay!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'minhaj123technado@gmail.com',
+    'password' => 'geeks&bachelors',
+    'port' => '587'
+  );
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
